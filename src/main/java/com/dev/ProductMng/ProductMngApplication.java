a3e176cb4f02e9a4ce3a9e91617c38ec9df1572d
GIT-SIK <xeno_s@naver.com>
2023-12-06 21:34:47 +0900

    Default

---
+++ src/main/java/com/dev/ProductMng/ProductMngApplication.java
@@ -0,0 +1,13 @@
+package com.dev.ProductMng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductMngApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductMngApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dev/ProductMng/ProductMngApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.ProductMng;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductMngApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
