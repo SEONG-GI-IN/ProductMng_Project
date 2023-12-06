dfa86044bccb0fb7793865e95141dff9788d6a6e
giin9879 <giin9879@gmail.com>
2023-12-07 00:30:45 +0900

    sb-admin / tiles 설정 커밋

--- src/main/java/com/dev/ProductMng/ProductMngApplication.java
+++ src/main/java/com/dev/ProductMng/ProductMngApplication.java
@@ -3,11 +3,20 @@ package com.dev.ProductMng;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.IOException;
+
 @SpringBootApplication
 public class ProductMngApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProductMngApplication.class, args);
+
+		// 브라우저를 열기 위해 브라우저를 열 수 있는 명령어를 실행
+		try {
+			Runtime.getRuntime().exec("rundll32 url.dll,FileProtocolHandler http://localhost:8080/");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
