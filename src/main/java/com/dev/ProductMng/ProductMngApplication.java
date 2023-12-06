1c8e48aa71a3e8281e2745ad1f0d74421d644c73
GIT-SIK <xeno_s@naver.com>
2023-12-07 05:34:54 +0900

    gradle :: log4j 사용하기 위해 starter 추가 main class :: catch 내용을 log로 대체

--- src/main/java/com/dev/ProductMng/ProductMngApplication.java
+++ src/main/java/com/dev/ProductMng/ProductMngApplication.java
@@ -2,20 +2,23 @@ package com.dev.ProductMng;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 
 @SpringBootApplication
 public class ProductMngApplication {
-
+	static Logger log = LoggerFactory.getLogger(ProductMngApplication.class);
 	public static void main(String[] args) {
+
 		SpringApplication.run(ProductMngApplication.class, args);
 
-		// 브라우저를 열기 위해 브라우저를 열 수 있는 명령어를 실행
+		// 브라우저를 열기 위해 브라우저를 열 수 있는 명령어를 실행 (Windows
 		try {
 			Runtime.getRuntime().exec("rundll32 url.dll,FileProtocolHandler http://localhost:8080/");
 		} catch (IOException e) {
-			e.printStackTrace();
+			log.warn("자동으로 브라우저를 실행할 수 없습니다.");
 		}
 	}
 
