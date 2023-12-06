213d191acbc822a9adae7155c876f678fc8c4743
GIT-SIK <xeno_s@naver.com>
2023-12-07 05:37:06 +0900

    불필요한 부분 제거

--- src/main/java/com/dev/ProductMng/ProductMngApplication.java
+++ src/main/java/com/dev/ProductMng/ProductMngApplication.java
@@ -14,7 +14,7 @@ public class ProductMngApplication {
 
 		SpringApplication.run(ProductMngApplication.class, args);
 
-		// 브라우저를 열기 위해 브라우저를 열 수 있는 명령어를 실행 (Windows
+		// 브라우저를 열기 위해 브라우저를 열 수 있는 명령어를 실행
 		try {
 			Runtime.getRuntime().exec("rundll32 url.dll,FileProtocolHandler http://localhost:8080/");
 		} catch (IOException e) {
