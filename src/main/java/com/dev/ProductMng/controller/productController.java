5a842ae3a55417a3445ff74683af0a6c0dea55db
GIT-SIK <xeno_s@naver.com>
2023-12-18 18:34:24 +0900

    controller :: Date -> LocalDate

--- src/main/java/com/dev/ProductMng/controller/productController.java
+++ src/main/java/com/dev/ProductMng/controller/productController.java
@@ -4,15 +4,11 @@ import org.json.simple.JSONObject;
 import org.json.simple.JSONArray;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.ModelAndView;
 
-import java.text.SimpleDateFormat;
+import java.time.LocalDate;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
-import java.util.Map;
 
 @Controller
 public class productController {
@@ -26,20 +22,19 @@ public class productController {
     @RequestMapping(value = ("/product/search"), method = {RequestMethod.GET})
     @ResponseBody
     public JSONArray productSearch(
-            @RequestParam(value = "date1", required=false) @DateTimeFormat(pattern="yyyy-MM-dd") Date date1,
-            @RequestParam(value= "date2", required=false) @DateTimeFormat(pattern="yyyy-MM-dd") Date date2,
+            @RequestParam(value = "date1", required=false) @DateTimeFormat(pattern="yyyy-MM-dd") LocalDate date1,
+            @RequestParam(value= "date2", required=false) @DateTimeFormat(pattern="yyyy-MM-dd") LocalDate date2,
             @RequestParam(value= "productCategories", required=false) String pc){
 
-        System.out.println(date1 +"<->"+ date2 +" / "+ pc);
-//       {'boardNum' : '99', spaceName : 'sn', Category: 'cate', price: 'price', regDate: 'regdate'};
+        System.out.println(date1 +" <-> "+ date2 +" / "+ pc);
 
         /* (TEST) JSON DATA */
         JSONArray array = new JSONArray();
         List<JSONObject> listObj = new ArrayList<>();
 
         /* (TEST) 날짜 포멧 */
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy년 MM월 dd일");
-        String nowDate = sdf.format(date1);
+//        SimpleDateFormat sdf = new SimpleDateFormat("yyyy년 MM월 dd일");
+//        String nowDate = sdf.format(date1);
 
         for(int i=0; i<10; i++) {
             JSONObject obj1 = new JSONObject();
@@ -47,7 +42,7 @@ public class productController {
             obj1.put("spaceName" , "sn" + i );
             obj1.put("Category" , pc);
             obj1.put("price" , "price"  + i);
-            obj1.put("regDate" , nowDate);
+            obj1.put("regDate" , date1);
             listObj.add(obj1);
 
         }
